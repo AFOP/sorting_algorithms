5b211eb1dc1ed16cec9dd6f6e81a7d076324654d
AFOP <4592@holbertonschool.com>
2022-06-21 20:03:38 -0500

    Update Task 0

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -11,24 +11,21 @@ void bubble_sort(int *array, size_t size)
 {
 	size_t i, step;
 
-	if (size <= 1 || array == NULL)
+	if (size == 0 || array == NULL)
 		return;
-	if (size > 2)
+	for (step = 0; step < size - 1; ++step)
 	{
-		for (step = 0; step < size - 1; ++step)
+		for (i = 0; i < size - step - 1; ++i)
 		{
-			for (i = 0; i < size - step - 1; ++i)
+			if (array[i] > array[i + 1])
 			{
-				if (array[i] > array[i + 1])
-				{
-					int temp = array[i];
+				int temp = array[i];
 
-					array[i] = array[i + 1];
+				array[i] = array[i + 1];
 
-					array[i + 1] = temp;
+				array[i + 1] = temp;
 
-					print_array(array, size);
-				}
+				print_array(array, size);
 			}
 		}
 	}

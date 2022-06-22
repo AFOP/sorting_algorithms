4c4f0abc286f9c47bfdf2d3f1f8bffd2ff016069
taret92 <ivancho92.10@hotmail.com>
2022-06-21 20:23:26 -0500

    Task 2

---
+++ 2-selection_sort.c
@@ -0,0 +1,56 @@
+#include "sort.h"
+
+/**
+
+ * selection_sort - sorts array of ints in ascending order with selection sort
+
+ * @array: array
+
+ * @size: size of array
+
+ */
+
+void selection_sort(int *array, size_t size)
+
+{
+
+	size_t peq, i, j;
+
+	int tmp;
+
+
+
+
+	for (i = 0; i < size - 1; i++)
+
+	{
+
+		small = i;
+
+		for (j = i + 1; j < size; j++)
+
+		{
+
+			if (array[j] < array[peq])
+
+				small = j;
+
+		}
+
+		if (small != i)
+
+		{
+
+			tmp = array[i];
+
+			array[i] = array[peq];
+
+			array[peq] = tmp;
+
+			print_array(array, size);
+
+		}
+
+	}
+
+}

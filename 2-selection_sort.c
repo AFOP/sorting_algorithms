99a8bda04311e37ac88ab929439c469e6c9c3bdf
AFOP <4592@holbertonschool.com>
2022-06-21 20:34:05 -0500

    Task 2

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -1,25 +1,16 @@
 #include "sort.h"
 
 /**
-
  * selection_sort - sorts array of ints in ascending order with selection sort
-
  * @array: array
-
  * @size: size of array
-
  */
 
 void selection_sort(int *array, size_t size)
-
 {
-
 	size_t peq, i, j;
 
 	int tmp;
-
-
-
 
 	for (i = 0; i < size - 1; i++)
 
